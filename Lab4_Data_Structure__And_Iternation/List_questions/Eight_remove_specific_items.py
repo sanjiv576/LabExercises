ef5adb4fbcff28162fe9e3e80782172b93127b33
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2021-08-17 18:08:21 +0545

    lab 4, data structue , questions added and updated again again

--- Lab4_Data_Structure__And_Iternation/List_questions/Eight_remove_specific_items.py
+++ Lab4_Data_Structure__And_Iternation/List_questions/Eight_remove_specific_items.py
@@ -1,9 +1,11 @@
 # Write a Python program to print a specified list after removing the 0th, 4th and 5th elements
 
 lst = ['remove1', 20, 30, 'bug', 'remove2', 'remove3', 4.23]
-an = lst
-for i in an:
-    show = an.index(i)
-    if show == 0 or show == 4 or show == 5:
-        out = lst.pop(show)
-    print(i)
+new_list = []
+for i in lst:
+
+    if i != lst[0] and i != lst[4] and i != lst[5]:
+        new_list.append(i)
+
+
+print(new_list)
